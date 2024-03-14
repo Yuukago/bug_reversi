aa521ebfe739d85667db4435734e4c5a55b69645
Yuukago <yuka.ago.work@gmail.com>
2024-03-14 23:19:56 +0900

    turnメソッドを修正

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -61,8 +61,7 @@ module ReversiMethods
   end
 
   def turn(board, target_pos, attack_stone_color, direction)
-    return false if target_pos.out_of_board?
-    return false if (target_pos.stone_color(board) == attack_stone_color) || (target_pos.stone_color(board) == '-' )
+    return false if target_pos.out_of_board? || [attack_stone_color, BLANK_CELL].include?(target_pos.stone_color(board))
 
     next_pos = target_pos.next_position(direction)
     if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
